0b2bfc26a3fb1b1b50a4b9efdb2dd2a6c6908f87
Kartik Mehra <52105073+Kartik-bit-web@users.noreply.github.com>
2022-10-21 11:57:10 +0000

    Update newData3.js

--- newData3.js
+++ newData3.js
@@ -1,2 +1,313 @@
-//Ghar Jarot - Pargod 
+//Ghar Jarot - Pargod - Durana
 
+{
+  'name': 'Pawan-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Parbhat- Baldoa',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Uttam- Baldoa',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'KamalJeet- Baldoa',
+  'O.B': 2418,
+  'Bill': 1774,
+  'Receive' 1000,
+  'Return': 0
+},
+  
+{
+  'name': 'Nirmal -Pargod',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Hardiyal- Pargod',
+  'O.B': 2912,
+  'Bill': 272,
+  'Receive' 100,
+  'Return': 0
+},
+  
+{
+  'name': 'Vikram-Challa',
+  'O.B': 700,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Shanti- Sawroop',
+  'O.B': 8033,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Anupama-Dafeda',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Govardhan-Har.',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Family Dhava- Har.',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ravinder- Har.',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rasesh-Har.',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Savita- Har.',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sunil Dhaba- Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'S.R Sons - Durana,
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Choudhary- Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rohit- Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rakesh-Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Manish- Baldoa',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Samer- Baldoa',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Mahadev- Har.',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'PS Chouhan',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Pawan- Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Gagan-Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Dev raj- Thana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Karnail-Bakan',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Harbans- Durana',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Subhash- kehrian',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kewal- Nadoli',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Veer Singh- Bharmada',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Manjeet- Kuther',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Vinay-Kuther',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Onkar- Kuther,
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'PatiaL Gift- Kuther',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Arjun- Taliyan',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ajay - Samyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Shamsher- Nandoli',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Patiyal- var',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}
