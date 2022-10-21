60c76f71e0d37486ee654b0467aec5e768edd937
Kartik Mehra <52105073+Kartik-bit-web@users.noreply.github.com>
2022-10-21 11:32:57 +0000

    Create newData2.js

---
+++ newData2.js
@@ -0,0 +1,345 @@
+//Jawali - GUGLARA
+{
+  'name': 'Ajay-Nagni Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Jasveer- Nagni Harser',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Vijay-Hawal',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Abhay Thappal',
+  'O.B': 2418,
+  'Bill': 1774,
+  'Receive' 1000,
+  'Return': 0
+},
+  
+{
+  'name': 'Kamal-Basantpur',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Kewal-Kehrian',
+  'O.B': 2912,
+  'Bill': 272,
+  'Receive' 100,
+  'Return': 0
+},
+  
+{
+  'name': 'Anup- kehrian',
+  'O.B': 700,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Jaggu- Dhan',
+  'O.B': 8033,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rajveer- Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Happy- Dhan',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Madan-farian',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Pawan-Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Vimala-Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ashok-Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Virender-Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rohit-Gurudawara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mukand-Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sukhlal- Samkehar',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Saksham-dan',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ranjeet-kehrian',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ram Farian',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Anil-Basantpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Gulshan-Samkehar',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kashmir Singh-Dugi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'G.S Choudhary-sam',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Muna Pong- Guglara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'ArjunRest- Guglara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Chaman-Guglara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Jagroop- Chalawara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Vika-Kardiyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rajat-Kardiyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Dhiman-Matlahar',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Yugal-Matlahar',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kannu-Jawali',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Abhi-Jawali',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mohit- Jawali',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sunil- Chalwara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ajay- Kardiyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sanjeev-Jawali',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Tim Tim- Jawali',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+  
+{
+  'name': 'Shiv-Jawali',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Baba',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}, 
+
+{
+  'name': 'Arjun- Taliyan',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}
