0a1dd99049aa5b055d3d76bc8db4fca6314b6281
Kartik Mehra <52105073+Kartik-bit-web@users.noreply.github.com>
2022-10-21 12:26:30 +0000

    Create newData4.js

---
+++ newData4.js
@@ -0,0 +1,393 @@
+//Nagrota Surian - Nandpur 
+{
+  'name': 'Pathania- Bassa',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Vijay(Pinku)- Bassa',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Guruvachan- Bassa,
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Joginder- Bassa',
+  'O.B': 2418,
+  'Bill': 1774,
+  'Receive' 1000,
+  'Return': 0
+},
+  
+{
+  'name': 'Sanjeev(Kalu)- Bassa',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Gopal Sharma- NS',
+  'O.B': 2912,
+  'Bill': 272,
+  'Receive' 100,
+  'Return': 0
+},
+  
+{
+  'name': 'Krishna Tea Stall- NS',
+  'O.B': 700,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mansa- NS',
+  'O.B': 8033,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Anjay Con.- NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'AS Con.- NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Manohar-NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mehra- NS,
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Dogra Con.- NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Satpal- NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ghunghar-Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Des Raj- Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rakesh- Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ashwani- Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Virender- Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ashok- Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Gulariya- NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Sandeep- Ablela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Beffra- Khabbal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Pushpinder- Ludret',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Jagat Ram- Ludret',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Raj Verma- Ludret',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rakesh- Ludret',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ramesh- Ludret',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Vidhi Chand- Ludret',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Gularia- Bariyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ramkesh- Bariyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Rajinder- Bhiyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Bhupender-Bhiyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kartar- Nandpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Virender- Nandpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Manish- Nandpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Vikesh- Nandpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rexi Dial- Nandpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sushil- Nandpur',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Nitu- Bariyal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+  
+{
+  'name': 'Ram- Katholi',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ashawani- Parkhar',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}, 
+
+{
+  'name': 'Surmi- Baldoa',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rajesh- Baldoa',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}, 
+
+{
+  'name': 'Sonu- Suknara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Joginder- Sukhnara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}, 
+
+{
+  'name': 'Vikram Rana- NS',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Andoria- Suknara',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}, 
+
+{
+  'name': 'Ajay- jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}
