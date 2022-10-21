25198e9c3c34d5aaa5e09819ef8c54395f49b354
Kartik Mehra <52105073+Kartik-bit-web@users.noreply.github.com>
2022-10-21 10:52:26 +0000

    Commit

--- newData.js
+++ newData.js
@@ -1,1 +1,496 @@
+{
+  'name': 'Sadanand-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
 
+{
+  'name': 'Ajay Dhiman-Harser',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Nushu-Harser',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Jagdish-Harser',
+  'O.B': 2418,
+  'Bill': 1774,
+  'Receive' 1000,
+  'Return': 0
+},
+  
+{
+  'name': 'Jai Pal-Harser',
+  'O.B': 0,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Vijay-Studio',
+  'O.B': 2912,
+  'Bill': 272,
+  'Receive' 100,
+  'Return': 0
+},
+  
+{
+  'name': 'Kunwar-Harser',
+  'O.B': 700,
+  'Bill': 0,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Gajai Singh-Harser',
+  'O.B': 8033,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Reena Con.-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Binduu-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Akhil-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kundan lal-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kuldeep-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sawroop-Harser',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Deepali-Panalath',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sudershan-Panalath',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Kishore-Dehri',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Suresh-Dehri',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'manjeet-Dehri',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rajesh-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Choudhary-Book',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Puria-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'G.N.T-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Anil-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Vinod-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Takdeer-Ghar Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mohan-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Harsh-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Thakur Gen. -Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sandeep-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Chet Ram-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Sanjeev-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Partap-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Manohar-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mankotia-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ajay-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ravi-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Chenchala-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rattan-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Paramjeet-Ghat',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+  
+{
+  'name': 'Surjeet-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Pawan-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+}, 
+
+{
+  'name': 'Parveen-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Subhash-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Mahinder-Amlela',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+
+{
+  'name': 'Jeevan Station-Jarpal',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Parshotam-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Subhash-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sudershan-jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Tilak-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Dev Raj-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Surinder-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Dev Raj-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Bihari lal-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ajeet-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Gurmeet-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Sanjay-Ked',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Rishabh-Dehri',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Ramesh-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'SomNath-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Toni-Jarot',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
+  
+{
+  'name': 'Surjeet-Pnalth',
+  'O.B': 14492,
+  'Bill': 4166,
+  'Receive' 0,
+  'Return': 0
+},
